f2829010de7b9b13ed2dbd8fc7f01fd543652d84
Karox1234 <zzzcomet@gmail.com>
2024-02-27 17:11:57 +0900

    초기설정

---
+++ src/main/kotlin/com/teamsparta/moamoa/MoamoaApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.moamoa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoamoaApplication
+
+fun main(args: Array<String>) {
+	runApplication<MoamoaApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/moamoa/MoamoaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.moamoa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoamoaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
