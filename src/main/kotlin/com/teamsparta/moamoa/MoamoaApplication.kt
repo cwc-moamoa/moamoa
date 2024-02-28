006c369d64a187a4d5883ae42e114b4b51740166
Karox1234 <zzzcomet@gmail.com>
2024-02-28 21:11:27 +0900

    추가 : @EnableJpaAuditing 추가

--- src/main/kotlin/com/teamsparta/moamoa/MoamoaApplication.kt
+++ src/main/kotlin/com/teamsparta/moamoa/MoamoaApplication.kt
@@ -3,8 +3,10 @@ package com.teamsparta.moamoa
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableCaching
+@EnableJpaAuditing
 @SpringBootApplication
 class MoamoaApplication
 
