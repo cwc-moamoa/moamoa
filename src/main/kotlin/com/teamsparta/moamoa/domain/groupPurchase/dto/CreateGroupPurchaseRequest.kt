b72ef416e4177ea78a8f385a1aaa29238d24f326
Karox1234 <zzzcomet@gmail.com>
2024-02-29 19:36:33 +0900

    수정 : id값 Long으로 수정

--- src/main/kotlin/com/teamsparta/moamoa/domain/groupPurchase/dto/CreateGroupPurchaseRequest.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/groupPurchase/dto/CreateGroupPurchaseRequest.kt
@@ -3,7 +3,7 @@ package com.teamsparta.moamoa.domain.groupPurchase.dto
 import java.time.LocalDateTime
 
 data class CreateGroupPurchaseRequest(
-    val productId: Int,
+    val productId: Long,
     val userLimit: Int,
     val timeLimit: LocalDateTime,
     val discount: Double
