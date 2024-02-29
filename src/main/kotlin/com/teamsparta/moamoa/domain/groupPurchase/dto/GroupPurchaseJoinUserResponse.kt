08b9bd5b18323e744aa8680ff24bd23d326f2bf4
Karox1234 <zzzcomet@gmail.com>
2024-02-29 17:02:34 +0900

    수정 : orderId 삭제

--- src/main/kotlin/com/teamsparta/moamoa/domain/groupPurchase/dto/GroupPurchaseJoinUserResponse.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/groupPurchase/dto/GroupPurchaseJoinUserResponse.kt
@@ -3,6 +3,5 @@ package com.teamsparta.moamoa.domain.groupPurchase.dto
 data class GroupPurchaseJoinUserResponse(
     val id: Long?,
     val userId: Long,
-    val orderId: Long,
     val groupPurchaseId: Long
 )
