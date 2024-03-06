2f8686dac6afbf377cd3ff2b0a7946ee186bbb31
dyorcat <yool0417@gmail.com>
2024-03-06 19:25:35 +0900

    수정 : UserServiceImpl 함수명 수정

--- src/main/kotlin/com/teamsparta/moamoa/domain/user/service/UserServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/user/service/UserServiceImpl.kt
@@ -4,7 +4,7 @@ import com.teamsparta.moamoa.domain.user.dto.UserResponse
 import com.teamsparta.moamoa.domain.user.dto.UserSignInRequest
 import com.teamsparta.moamoa.domain.user.dto.UserSignInResponse
 import com.teamsparta.moamoa.domain.user.dto.UserSignUpRequest
-import com.teamsparta.moamoa.domain.user.exception.InvalidCredentialException
+import com.teamsparta.moamoa.exception.InvalidCredentialException
 import com.teamsparta.moamoa.domain.user.repository.UserRepository
 import com.teamsparta.moamoa.exception.ModelNotFoundException
 import com.teamsparta.moamoa.infra.security.jwt.JwtPlugin
@@ -12,54 +12,48 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.ui.Model
 import java.time.LocalDateTime
 
 @Service
 class UserServiceImpl(
     private val userRepository: UserRepository,
     private val passwordEncoder: PasswordEncoder,
-    private val jwtPlugin: JwtPlugin
-): UserService {
-
+    private val jwtPlugin: JwtPlugin,
+) : UserService {
     @Transactional
-    override fun userSignUp(userSignUpRequest: UserSignUpRequest): UserResponse {
-        if (userRepository.existsByEmail(userSignUpRequest.email))
+    override fun signUpUser(userSignUpRequest: UserSignUpRequest): UserResponse {
+        if (userRepository.existsByEmail(userSignUpRequest.email)) {
             throw IllegalStateException("Email is already in use")
-        if(userSignUpRequest.passwordCheck != userSignUpRequest.password) throw InvalidCredentialException()
+        }
+        if (userSignUpRequest.passwordCheck != userSignUpRequest.password) throw InvalidCredentialException()
         val user = userRepository.save(userSignUpRequest.toEntity(passwordEncoder))
         return UserResponse.toResponse(user)
     }
 
     @Transactional
-    override fun userSignIn(userSignInRequest: UserSignInRequest): UserSignInResponse {
+    override fun signInUser(userSignInRequest: UserSignInRequest): UserSignInResponse {
         val user = userRepository.findByEmail(userSignInRequest.email) ?: throw ModelNotFoundException("User", null)
-        if(!passwordEncoder.matches(userSignInRequest.password, user.password)) throw InvalidCredentialException()
+        if (!passwordEncoder.matches(userSignInRequest.password, user.password)) throw InvalidCredentialException()
         return UserSignInResponse(
-            accessToken = jwtPlugin.generateAccessToken(
-                subject = user.id.toString(),
-                email = user.email,
-            )
+            accessToken =
+                jwtPlugin.generateAccessToken(
+                    subject = user.id.toString(),
+                    email = user.email,
+                ),
         )
     }
 
-    override fun userDelete(userId: Long): UserResponse {
+    override fun deleteUser(userId: Long): UserResponse {
         val user = userRepository.findByIdOrNull(userId) ?: throw ModelNotFoundException("User", userId)
 
-        if(userId != user.id) {
-            throw InvalidCredentialException()
+        if (userId != user.id) {
+            throw InvalidCredentialException() // 다른 예외로 바꿔야 할 듯!
         }
         user.deletedAt = LocalDateTime.now()
         userRepository.save(user)
 
         return UserResponse.toResponse(user)
 
-        //진행중입니다~~~!!
-
-
-
+        // 진행중입니다~~~!!
     }
-
-
-
 }
