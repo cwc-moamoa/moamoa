ba1e0e3340f1092c183ffb7f4368ad9115ef3eef
dyorcat <yool0417@gmail.com>
2024-03-06 19:23:36 +0900

    수정: JwtPlugIn ISSUER, SECRET, ACCESS_TOKEN_EXPRIRATION_HOUR 을 companion object -> Value 어노테이션으로 수정

--- src/main/kotlin/com/teamsparta/moamoa/infra/security/jwt/JwtPlugin.kt
+++ src/main/kotlin/com/teamsparta/moamoa/infra/security/jwt/JwtPlugin.kt
@@ -12,44 +12,46 @@ import java.time.Instant
 import java.util.*
 
 @Component
-class JwtPlugin{
-    companion object {
-        const val ISSUER = "team.sparta.com"
-        const val SECRET = "PO4c8z41Hia5gJG3oeuFJMRYBB4Ws4aZ"
-        const val ACCESS_TOKEN_EXPIRATION_HOUR: Long = 168
-    }
+class JwtPlugin(
+    @Value("\${auth.jwt.issuer}") private val issuer: String,
+    @Value("\${auth.jwt.secret}") private val secret: String,
+    @Value("\${auth.jwt.accessTokenExpirationHour}") private val accessTokenExpirationHour: Long,
+) {
 
     fun validateToken(jwt: String): Result<Jws<Claims>> {
         return kotlin.runCatching {
-            val key = Keys.hmacShaKeyFor(SECRET.toByteArray(StandardCharsets.UTF_8))
-            // key로 서명을 검증하고, 만료시간을 체크
+            val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
             Jwts.parser().verifyWith(key).build().parseSignedClaims(jwt)
         }
     }
 
-    fun generateAccessToken(subject: String, email: String): String {
-        return generateToken(subject, email, Duration.ofHours(ACCESS_TOKEN_EXPIRATION_HOUR))
-
+    fun generateAccessToken(
+        subject: String,
+        email: String,
+    ): String {
+        return generateToken(subject, email, Duration.ofHours(accessTokenExpirationHour))
     }
 
-    private fun generateToken(subject: String, email: String, expirationPeriod: Duration): String {
-        val claims: Claims = Jwts.claims()
-            .add(mapOf("email" to email))
-            .build()
+    private fun generateToken(
+        subject: String,
+        email: String,
+        expirationPeriod: Duration,
+    ): String {
+        val claims: Claims =
+            Jwts.claims()
+                .add(mapOf("email" to email))
+                .build()
 
-        val key = Keys.hmacShaKeyFor(SECRET.toByteArray(StandardCharsets.UTF_8))
+        val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
         val now = Instant.now()
 
         return Jwts.builder()
             .subject(subject)
-            .issuer(ISSUER)
+            .issuer(issuer)
             .issuedAt(Date.from(now))
             .expiration(Date.from(now.plus(expirationPeriod)))
             .claims(claims)
             .signWith(key)
             .compact()
     }
-
-
-
 }
