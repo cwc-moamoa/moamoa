1dc54679186d4d6f34fd7b38bdb1717a0242f10b
Karox1234 <zzzcomet@gmail.com>
2024-03-06 19:56:24 +0900

    추가 : 테스트용 레디스에 데이터 저장 추가

--- src/main/kotlin/com/teamsparta/moamoa/domain/groupPurchase/controller/GroupPurchaseController.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/groupPurchase/controller/GroupPurchaseController.kt
@@ -4,11 +4,7 @@ import com.teamsparta.moamoa.domain.groupPurchase.dto.CreateGroupPurchaseRequest
 import com.teamsparta.moamoa.domain.groupPurchase.service.GroupPurchaseService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/groupPurchases")
@@ -26,8 +22,9 @@ class GroupPurchaseController(private val groupPurchaseService: GroupPurchaseSer
     fun createAndJoinOrJoin(
         @RequestBody request: CreateGroupPurchaseRequest,
         @RequestParam userId: Long,
+        @RequestParam orderId: Long,
     ): ResponseEntity<String> {
-        groupPurchaseService.createAndJoinOrJoinGroupPurchase(request, userId)
+        groupPurchaseService.createAndJoinOrJoinGroupPurchase(request, userId, orderId)
         return ResponseEntity.status(HttpStatus.OK).body("공동구매 매칭 성공")
     }
 
@@ -35,17 +32,28 @@ class GroupPurchaseController(private val groupPurchaseService: GroupPurchaseSer
     fun join(
         @RequestParam userId: Long,
         @RequestParam groupPurchaseId: Long,
+        @RequestParam orderId: Long,
     ): ResponseEntity<Unit> {
-        val groupPurchaseJoinUserResponse = groupPurchaseService.joinGroupPurchase(userId, groupPurchaseId)
+        val groupPurchaseJoinUserResponse = groupPurchaseService.joinGroupPurchase(userId, groupPurchaseId, orderId)
         return ResponseEntity.status(HttpStatus.OK).body(groupPurchaseJoinUserResponse)
     }
 
-    @PostMapping("/leave")
+    @PutMapping("/leave")
     fun leave(
         @RequestParam userId: Long,
         @RequestParam groupPurchaseId: Long,
     ): ResponseEntity<Unit> {
         groupPurchaseService.leaveGroupPurchase(userId, groupPurchaseId)
+        return ResponseEntity.status(HttpStatus.OK).build()
+    }
+
+    @PostMapping("/saveToRedis")
+    fun saveDataToRedis(
+        @RequestParam productId: String,
+        @RequestParam userId: String,
+        @RequestParam orderId: String,
+    ): ResponseEntity<Unit> {
+        groupPurchaseService.saveToRedis(productId, userId, orderId) // 여기 순서 중요함!!
         return ResponseEntity.status(HttpStatus.OK).build()
     }
 }
