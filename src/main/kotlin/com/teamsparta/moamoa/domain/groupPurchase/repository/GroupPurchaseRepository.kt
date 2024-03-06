5fba18d03f6466cf0bae63468a2c0a3872925580
Karox1234 <zzzcomet@gmail.com>
2024-03-06 19:55:36 +0900

    수정 : 쿼리삭제

--- src/main/kotlin/com/teamsparta/moamoa/domain/groupPurchase/repository/GroupPurchaseRepository.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/groupPurchase/repository/GroupPurchaseRepository.kt
@@ -2,17 +2,9 @@ package com.teamsparta.moamoa.domain.groupPurchase.repository
 
 import com.teamsparta.moamoa.domain.groupPurchase.model.GroupPurchaseEntity
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 
 interface GroupPurchaseRepository : JpaRepository<GroupPurchaseEntity, Long> {
-    @Query("SELECT g FROM GroupPurchaseEntity g WHERE g.productId = :productId AND g.deletedAt IS NULL")
-    fun findByProductId(
-        @Param("productId") productId: Long,
-    ): GroupPurchaseEntity?
+    fun findByProductIdAndDeletedAtIsNull(productId: Long): GroupPurchaseEntity?
 
-    @Query("SELECT g FROM GroupPurchaseEntity g WHERE g.id = :groupPurchaseId AND g.deletedAt IS NULL")
-    fun findByIdOrNull(
-        @Param("groupPurchaseId") groupPurchaseId: Long,
-    ): GroupPurchaseEntity?
+    fun findByIdAndDeletedAtIsNull(groupPurchaseId: Long): GroupPurchaseEntity?
 }
