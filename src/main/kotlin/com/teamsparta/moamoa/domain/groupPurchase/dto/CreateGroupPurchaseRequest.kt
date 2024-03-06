999fa54dd9dfc833054f478605bef05c169aedb7
Karox1234 <zzzcomet@gmail.com>
2024-03-06 19:57:09 +0900

    수정 : request에서 productid 삭제,redis에서 직접 찾아옴

--- src/main/kotlin/com/teamsparta/moamoa/domain/groupPurchase/dto/CreateGroupPurchaseRequest.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/groupPurchase/dto/CreateGroupPurchaseRequest.kt
@@ -3,7 +3,6 @@ package com.teamsparta.moamoa.domain.groupPurchase.dto
 import java.time.LocalDateTime
 
 data class CreateGroupPurchaseRequest(
-    val productId: Long,
     val userLimit: Int,
     val timeLimit: LocalDateTime,
     val discount: Double,
