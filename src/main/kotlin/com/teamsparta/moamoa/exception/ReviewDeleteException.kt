c89e3742541bc6c7ea488b7cc150721106919c7c
marygang <gangmary2@gmail.com>
2024-03-07 11:13:10 +0900

    수정, 추가 1. DeletedAt 수정 (service, repository) 2. Review 조회 기능 추가

--- src/main/kotlin/com/teamsparta/moamoa/exception/ReviewDeleteException.kt
+++
@@ -1,8 +0,0 @@
-package com.teamsparta.moamoa.exception
-
-data class ReviewDeleteException(
-    var reviewId: Long,
-) :
-    RuntimeException(
-            "Deleted review with ID $reviewId cannot be updated.",
-        )
