10e7b4df055c259728a5ad686375cf08ccb713f9
dyorcat <yool0417@gmail.com>
2024-03-08 14:36:30 +0900

    추가 : OAuth2LoginSuccessHandler 추가

---
+++ src/main/kotlin/com/teamsparta/moamoa/domain/socialUser/service/OAuth2LoginSuccessHandler.kt
@@ -0,0 +1,26 @@
+package com.teamsparta.moamoa.domain.socialUser.service
+
+import com.teamsparta.moamoa.domain.socialUser.dto.OAuth2UserInfo
+import com.teamsparta.moamoa.domain.socialUser.JwtHelper
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+import org.springframework.stereotype.Component
+
+@Component
+class OAuth2LoginSuccessHandler(
+    private val jwtHelper: JwtHelper
+) : AuthenticationSuccessHandler {
+
+    override fun onAuthenticationSuccess(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authentication: Authentication
+    ) {
+        val userInfo = authentication.principal as OAuth2UserInfo
+        val accessToken = jwtHelper.generateAccessToken(userInfo.id)
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.writer.write(accessToken) }
+}
