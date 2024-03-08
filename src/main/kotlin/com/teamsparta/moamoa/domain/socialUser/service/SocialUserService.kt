fa7b064b79a1f0a547661756bf0e2a957dbe3f8c
dyorcat <yool0417@gmail.com>
2024-03-08 14:36:09 +0900

    추가 : SocialUserService 추가

---
+++ src/main/kotlin/com/teamsparta/moamoa/domain/socialUser/service/SocialUserService.kt
@@ -0,0 +1,23 @@
+package com.teamsparta.moamoa.domain.socialUser.service
+
+import com.teamsparta.moamoa.domain.socialUser.dto.OAuth2UserInfo
+import com.teamsparta.moamoa.domain.socialUser.model.OAuth2Provider
+import com.teamsparta.moamoa.domain.socialUser.model.SocialUser
+import com.teamsparta.moamoa.domain.socialUser.repository.SocialUserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class SocialUserService(
+    private val socialUserRepository: SocialUserRepository
+) {
+
+    fun registerIfAbsent(userInfo: OAuth2UserInfo): SocialUser {
+        val provider = OAuth2Provider.valueOf(userInfo.provider)
+        return if (!socialUserRepository.existsByProviderAndProviderId(provider, userInfo.id)) {
+            val socialMember = SocialUser.ofKakao(userInfo.id, userInfo.nickname)
+            socialUserRepository.save(socialMember)
+        } else {
+            socialUserRepository.findByProviderAndProviderId(provider, userInfo.id)
+        }
+    }
+}
