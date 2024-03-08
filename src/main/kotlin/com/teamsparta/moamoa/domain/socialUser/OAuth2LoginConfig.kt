6213d7520b01935609d7ca04a4852f306f6fc452
dyorcat <yool0417@gmail.com>
2024-03-08 14:35:40 +0900

    추가 : OAuth2LoginConfig 추가

---
+++ src/main/kotlin/com/teamsparta/moamoa/domain/socialUser/OAuth2LoginConfig.kt
@@ -0,0 +1,36 @@
+package com.teamsparta.moamoa.domain.socialUser
+
+import com.teamsparta.moamoa.domain.socialUser.service.OAuth2LoginSuccessHandler
+import com.teamsparta.moamoa.domain.socialUser.service.OAuth2UserService
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class OAuth2LoginConfig(
+    private val oAuth2UserService: OAuth2UserService,
+    private val oAuth2LoginSuccessHandler: OAuth2LoginSuccessHandler
+) {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http.formLogin { it.disable() }
+            .httpBasic { it.disable() }
+            .csrf { it.disable() }
+            .cors { it.disable() }
+            .headers { it.frameOptions { options -> options.sameOrigin() } }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }.oauth2Login { oauthConfig ->
+                oauthConfig.authorizationEndpoint {
+                    it.baseUri("/oauth2/login") // /oauth2/login/kakao
+                }.redirectionEndpoint {
+                    it.baseUri("/oauth2/callback/*") // /oauth2/callback/kakao
+                }.userInfoEndpoint {
+                    it.userService(oAuth2UserService)
+                }.successHandler(oAuth2LoginSuccessHandler)
+            }.build()
+    }
+}
