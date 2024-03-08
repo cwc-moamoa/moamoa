07c117958b3e80a9ee258e585b47c63fd4c3497b
dyorcat <yool0417@gmail.com>
2024-03-08 17:45:26 +0900

    수정 : JwtHelper 수정

--- src/main/kotlin/com/teamsparta/moamoa/domain/socialUser/JwtHelper.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/socialUser/JwtHelper.kt
@@ -1,11 +1,13 @@
 package com.teamsparta.moamoa.domain.socialUser
 
+import com.teamsparta.moamoa.infra.security.jwt.JwtPlugin
 import org.springframework.stereotype.Component
 
 @Component
-class JwtHelper {
-
-    fun generateAccessToken(id: String): String {
-        return "SampleAccessToken $id"
+class JwtHelper(
+    private val jwtPlugin: JwtPlugin
+) {
+    fun generateAccessToken(subject: String, nickname: String, email: String): String {
+        return jwtPlugin.generateAccessToken(subject, nickname, email)
     }
 }
