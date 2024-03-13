fce5493af8d7d8cf62af7066d635965d2614e22e
Karox1234 <zzzcomet@gmail.com>
2024-03-13 14:39:15 +0900

    수정 : 로직 일부 수정

--- src/main/kotlin/com/teamsparta/moamoa/domain/groupPurchase/service/GroupPurchaseService.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/groupPurchase/service/GroupPurchaseService.kt
@@ -1,10 +1,10 @@
 package com.teamsparta.moamoa.domain.groupPurchase.service
 
-import com.teamsparta.moamoa.domain.groupPurchase.dto.*
 import com.teamsparta.moamoa.domain.groupPurchase.model.GroupPurchaseEntity
 import com.teamsparta.moamoa.domain.groupPurchase.model.GroupPurchaseJoinUserEntity
 import com.teamsparta.moamoa.domain.groupPurchase.repository.GroupPurchaseJoinUserRepository
 import com.teamsparta.moamoa.domain.groupPurchase.repository.GroupPurchaseRepository
+import com.teamsparta.moamoa.domain.product.repository.ProductRepository
 import com.teamsparta.moamoa.exception.ModelNotFoundException
 import jakarta.transaction.Transactional
 import org.springframework.data.redis.core.RedisTemplate
@@ -16,38 +16,39 @@ class GroupPurchaseService(
     private val groupPurchaseRepository: GroupPurchaseRepository,
     private val groupPurchaseJoinUserRepository: GroupPurchaseJoinUserRepository,
     private val redisTemplate: RedisTemplate<String, Any>,
+    private val productRepository: ProductRepository,
 ) {
-//    @Transactional
-//    fun createAndJoinOrJoinGroupPurchase(
-//        request: CreateGroupPurchaseRequest, // 추후 product 완료시 정보를 자동으로 받아오게 변경
-//        userId: Long, // 추후 order 완료시 정보를 자동으로 받아오게 변경
-////        orderId: Long, // 추후 order 완료시 정보를 자동으로 받아오게 변경
-//    ) {
-//        val orderId = // 방금 끝난 주문의 orderId 를 받아옴
-//        val uniqueOrderId = redisTemplate.opsForHash<String, String>().get(orderId.toString(), "productId") ?: throw Exception("redis에 정보가 없다 애송이")
-//        val productId = uniqueOrderId.toLong()
-//        val groupPurchase = groupPurchaseRepository.findByProductIdAndDeletedAtIsNull(productId)
-//
-//        if (groupPurchase == null) {
-//            val newGroupPurchase =
-//                GroupPurchaseEntity(
-//                    productId,
-//                    request.userLimit,
-//                    1,
-//                    request.timeLimit.plusHours(24),
-//                    request.discount,
-//                    mutableListOf(),
-//                )
-//
-//            val groupPurchaseJoinUser = GroupPurchaseJoinUserEntity(userId, newGroupPurchase, order.id)
-//
-//            newGroupPurchase.groupPurchaseUsers.add(groupPurchaseJoinUser)
-//            groupPurchaseRepository.save(newGroupPurchase)
-//            redisTemplate.delete(orderId.toString())
-//        } else {
-//            joinGroupPurchase(userId, groupPurchase.id!!, orderId)
-//        }
-//    }
+    @Transactional
+    fun createAndJoinOrJoinGroupPurchase(
+        userId: String,
+        orderId: String,
+    ) {
+        val uniqueOrderId = redisTemplate.opsForHash<String, String>().get(orderId, "productId") ?: throw Exception("redis에 정보가 없습니다")
+        val productId = uniqueOrderId.toLong()
+        val groupPurchase = groupPurchaseRepository.findByProductIdAndDeletedAtIsNull(productId)
+        val findProduct =
+            productRepository.findById(productId)
+                .orElseThrow { ModelNotFoundException("product", productId) }
+
+        if (groupPurchase == null) {
+            val newGroupPurchase =
+                GroupPurchaseEntity(
+                    productId,
+                    findProduct.userLimit,
+                    1,
+                    findProduct.timeLimit,
+                    mutableListOf(),
+                )
+
+            val groupPurchaseJoinUser = GroupPurchaseJoinUserEntity(userId.toLong(), newGroupPurchase, uniqueOrderId.toLong())
+
+            newGroupPurchase.groupPurchaseUsers.add(groupPurchaseJoinUser)
+            groupPurchaseRepository.save(newGroupPurchase)
+            redisTemplate.delete(orderId)
+        } else {
+            joinGroupPurchase(userId.toLong(), groupPurchase.id!!, orderId.toLong())
+        }
+    }
 
     fun joinGroupPurchase(
         userId: Long,
@@ -59,12 +60,12 @@ class GroupPurchaseService(
                 "GroupPurchase", groupPurchaseId,
             )
         if (groupPurchase.userCount >= groupPurchase.userLimit) {
-            throw IllegalStateException("GroupPurchase is full")
+            throw Exception("GroupPurchase is full")
         }
 
         val existingJoinUser = groupPurchaseJoinUserRepository.findByUserIdAndGroupPurchaseId(userId, groupPurchaseId)
         if (existingJoinUser != null) {
-            throw IllegalStateException("이미 신청한 공동구매 입니다.")
+            throw Exception("이미 신청한 공동구매 입니다.")
         }
 
         val groupPurchaseJoinUser = GroupPurchaseJoinUserEntity(userId, groupPurchase, orderId)
@@ -114,22 +115,22 @@ class GroupPurchaseService(
         groupPurchaseJoinUserRepository.save(groupPurchaseJoinUser)
     }
 
-    fun GroupPurchaseEntity.toResponse(): GroupPurchaseResponse {
-        return GroupPurchaseResponse(
-            id = id!!,
-            productId = productId,
-            userLimit = userLimit,
-            userCount = userCount,
-            timeLimit = timeLimit,
-            discount = discount,
-        )
-    }
-
-    fun GroupPurchaseJoinUserEntity.toResponse(): GroupPurchaseJoinUserResponse {
-        return GroupPurchaseJoinUserResponse(
-            id = id!!,
-            userId = userId,
-            groupPurchaseId = groupPurchase.id!!,
-        )
-    }
+//    fun GroupPurchaseEntity.toResponse(): GroupPurchaseResponse {
+//        return GroupPurchaseResponse(
+//            id = id!!,
+//            productId = productId,
+//            userLimit = userLimit,
+//            userCount = userCount,
+//            timeLimit = timeLimit,
+//
+//        )
+//    }
+//
+//    fun GroupPurchaseJoinUserEntity.toResponse(): GroupPurchaseJoinUserResponse {
+//        return GroupPurchaseJoinUserResponse(
+//            id = id!!,
+//            userId = userId,
+//            groupPurchaseId = groupPurchase.id!!,
+//        )
+//    }
 }
