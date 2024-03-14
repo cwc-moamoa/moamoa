13703144b11b53a290eb3d91e0fc243d09b795b1
홍성욱 <suh75321@naver.com>
2024-03-14 10:29:59 +0900

    수정: "Operation으로 설명 추가"

--- src/main/kotlin/com/teamsparta/moamoa/domain/like/controller/LikeController.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/like/controller/LikeController.kt
@@ -1,6 +1,8 @@
 package com.teamsparta.moamoa.domain.like.controller
 
 import com.teamsparta.moamoa.domain.like.service.LikeService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -8,35 +10,36 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/likes")
 class LikeController(private val likeService: LikeService) {
+    @Operation(summary = "상품에 '좋아요' 추가", description = "특정 상품에 대한 사용자의 '좋아요'를 추가합니다.")
     @PostMapping("/product/{productId}/socialUser/{socialUserId}")
     fun addLikeToProduct(
-        @PathVariable productId: Long,
-        @PathVariable socialUserId: Long,
+        @Parameter(description = "상품 ID")@PathVariable productId: Long,
+        @Parameter(description = "소셜 사용자 ID")@PathVariable socialUserId: Long,
     ) {
         likeService.addLikeToProduct(productId, socialUserId)
     }
-
+    @Operation(summary = "상품에서 '좋아요' 제거", description = "특정 상품에 대한 사용자의 '좋아요'를 제거합니다.")
     @DeleteMapping("/product/{productId}/socialUser/{socialUserId}")
     fun removeLikeFromProduct(
-        @PathVariable productId: Long,
-        @PathVariable socialUserId: Long,
+        @Parameter(description = "상품 ID")@PathVariable productId: Long,
+        @Parameter(description = "소셜 사용자 ID")@PathVariable socialUserId: Long,
     ): ResponseEntity<String> {
         likeService.removeLikeFromProduct(productId, socialUserId)
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
     }
-
+    @Operation(summary = "리뷰에 '좋아요' 추가", description = "특정 리뷰에 대한 사용자의 '좋아요'를 추가합니다.")
     @PostMapping("/review/{reviewId}/socialUser/{socialUserId}")
     fun addLikeToReview(
-        @PathVariable reviewId: Long,
-        @PathVariable socialUserId: Long,
+        @Parameter(description = "리뷰 ID") @PathVariable reviewId: Long,
+        @Parameter(description = "소셜 사용자 ID") @PathVariable socialUserId: Long,
     ) {
         likeService.addLikeToReview(reviewId, socialUserId)
     }
-
+    @Operation(summary = "리뷰에서 '좋아요' 제거", description = "특정 리뷰에 대한 사용자의 '좋아요'를 제거합니다.")
     @DeleteMapping("/review/{reviewId}/socialUser/{socialUserId}")
     fun removeLikeFromReview(
-        @PathVariable reviewId: Long,
-        @PathVariable socialUserId: Long,
+        @Parameter(description = "리뷰 ID") @PathVariable reviewId: Long,
+        @Parameter(description = "소셜 사용자 ID") @PathVariable socialUserId: Long,
     ): ResponseEntity<String> {
         likeService.removeLikeFromReview(reviewId, socialUserId)
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
--- src/main/kotlin/com/teamsparta/moamoa/domain/product/controller/ProductController.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/product/controller/ProductController.kt
@@ -4,6 +4,8 @@ import com.teamsparta.moamoa.domain.product.dto.ProductRequest
 import com.teamsparta.moamoa.domain.product.dto.ProductResponse
 import com.teamsparta.moamoa.domain.product.service.ProductService
 import com.teamsparta.moamoa.infra.security.UserPrincipal
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
@@ -17,18 +19,19 @@ import org.springframework.web.bind.annotation.*
 class ProductController(
     private val productService: ProductService,
 ) {
+    @Operation(summary = "모든 상품 조회", description = "시스템에 등록된 모든 상품의 목록을 조회합니다.")
     @GetMapping
     fun getAllProducts(): List<ProductResponse> {
         return productService.getAllProducts()
     }
-
+    @Operation(summary = "상품 상세 조회", description = "주어진 상품 ID에 해당하는 상품의 상세 정보를 조회합니다.")
     @GetMapping("/{productId}")
     fun getProduct(
         @PathVariable productId: Long,
     ): ProductResponse {
         return productService.getProductById(productId)
     }
-
+    @Operation(summary = "상품 등록", description = "새로운 상품을 시스템에 등록합니다.")
     @PostMapping("/products")
     fun createProduct(
         @AuthenticationPrincipal user: UserPrincipal,
@@ -40,11 +43,11 @@ class ProductController(
         val response = ProductResponse(product)
         return ResponseEntity.ok(response)
     }
-
+    @Operation(summary = "상품 정보 업데이트", description = "주어진 상품 ID에 해당하는 상품의 정보를 업데이트합니다.")
     @PutMapping("/{productId}")
     fun updateProduct(
         @AuthenticationPrincipal user: UserPrincipal,
-        @PathVariable productId: Long,
+        @Parameter(description = "상품 ID") @PathVariable productId: Long,
 //        @PathVariable sellerId: Long,
         @RequestBody productRequest: ProductRequest,
     ): ResponseEntity<ProductResponse> {
@@ -52,21 +55,21 @@ class ProductController(
         //val updatedProduct = productService.updateProduct(productId, sellerId, productRequest)
         return ResponseEntity.ok(ProductResponse(updatedProduct))
     }
-
+    @Operation(summary = "상품 삭제", description = "주어진 상품 ID에 해당하는 상품을 시스템에서 삭제합니다.")
     @PutMapping("/{productId}/delete")
     fun deleteProduct(
         @AuthenticationPrincipal user: UserPrincipal,
-        @PathVariable productId: Long,
+        @Parameter(description = "상품 ID") @PathVariable productId: Long,
 //        @PathVariable sellerId: Long,
     ): ResponseEntity<ProductResponse> {
         val product = productService.deleteProduct(productId, user.id)
        // val product = productService.deleteProduct(productId, sellerId)
         return ResponseEntity.ok(ProductResponse(product))
     }
-
+    @Operation(summary = "페이지네이션을 통한 상품 목록 조회", description = "주어진 페이지네이션 정보에 따라 상품 목록을 조회합니다.")
     @GetMapping("/pages")
     fun getPaginatedProductList(
-        @PageableDefault(size = 15, sort = ["id"]) pageable: Pageable,
+        @Parameter(description = "페이지네이션 정보") @PageableDefault(size = 15, sort = ["id"]) pageable: Pageable,
     ): ResponseEntity<Page<ProductResponse>> {
         val products = productService.getPaginatedProductList(pageable)
         return ResponseEntity.status(HttpStatus.OK).body(products)
