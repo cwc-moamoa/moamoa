aba961796937a8905cb892236990a8032af5a991
Karox1234 <zzzcomet@gmail.com>
2024-03-19 12:18:00 +0900

    추가 : 논리삭제 검색을 위한 메서드 추가

--- src/main/kotlin/com/teamsparta/moamoa/domain/seller/repository/SellerRepository.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/seller/repository/SellerRepository.kt
@@ -2,9 +2,12 @@ package com.teamsparta.moamoa.domain.seller.repository
 
 import com.teamsparta.moamoa.domain.seller.model.Seller
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface SellerRepository : JpaRepository<Seller, Long> {
     fun existsByEmail(email: String): Boolean
 
     fun findByEmail(email: String): Seller?
+
+    fun findByIdAndDeletedAtIsNull(sellerId: Long) : Optional<Seller>
 }
