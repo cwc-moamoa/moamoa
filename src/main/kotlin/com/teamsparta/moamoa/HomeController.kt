a9e2601e1171daceda86e593f27524d2b9d7a320
Karox1234 <zzzcomet@gmail.com>
2024-03-20 21:42:01 +0900

    수정 : html 테스트 1차

--- src/main/kotlin/com/teamsparta/moamoa/HomeController.kt
+++ src/main/kotlin/com/teamsparta/moamoa/HomeController.kt
@@ -2,11 +2,16 @@ package com.teamsparta.moamoa
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class HomeController {
     @GetMapping("/start8080")
     fun home(): String {
         return "index"
+    }
+    @GetMapping("/order")
+    fun goToOrderPage(@RequestParam productId: String): String {
+        return "order"
     }
 }
