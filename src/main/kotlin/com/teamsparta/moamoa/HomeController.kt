1e028c8533a3a6a6dd9d25bc285d839b5a62f5a2
Karox1234 <zzzcomet@gmail.com>
2024-03-21 15:09:51 +0900

    수정 : html 테스트 2차

--- src/main/kotlin/com/teamsparta/moamoa/HomeController.kt
+++ src/main/kotlin/com/teamsparta/moamoa/HomeController.kt
@@ -1,6 +1,7 @@
 package com.teamsparta.moamoa
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 
@@ -13,5 +14,10 @@ class HomeController {
     @GetMapping("/order")
     fun goToOrderPage(@RequestParam productId: String): String {
         return "order"
+    }
+
+    @GetMapping("/product-detail")
+    fun goToProductDetail(@RequestParam productId: String) : String {
+        return "product-detail"
     }
 }
