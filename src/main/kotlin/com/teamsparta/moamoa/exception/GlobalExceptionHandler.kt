e9e33c97ecfd3bbef1195ef9035cf9603baa8bf7
dyorcat <yool0417@gmail.com>
2024-03-28 11:35:52 +0900

    GlobalExceptionHandler에서 에러발생시 logging 처리 추가

--- src/main/kotlin/com/teamsparta/moamoa/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/teamsparta/moamoa/exception/GlobalExceptionHandler.kt
@@ -1,6 +1,8 @@
 package com.teamsparta.moamoa.exception
 
 import com.teamsparta.moamoa.exception.dto.ErrorResponseDto
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+
+    private val logger: Logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
     @ExceptionHandler(IllegalStateException::class)
     fun handlerIllegalStateException(e: IllegalStateException): ResponseEntity<ErrorResponseDto> {
+        logger.error("IllegalStateException 발생", e)
         return ResponseEntity
             .status(HttpStatus.CONFLICT)
             .body(ErrorResponseDto(e.message))
@@ -18,11 +23,13 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(ModelNotFoundException::class)
     fun handleModeNotFoundException(e: ModelNotFoundException): ResponseEntity<ErrorResponseDto> {
+        logger.error("ModelNotFoundException 발생", e)
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponseDto(e.message))
     }
 
     @ExceptionHandler(IllegalArgumentException::class)
     fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorResponseDto> {
+        logger.error("IllegalArgumentException 발생", e)
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
             ErrorResponseDto(
                 e.message,
@@ -32,6 +39,7 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidCredentialException::class)
     fun handleInvalidCredentialException(e: InvalidCredentialException): ResponseEntity<ErrorResponseDto> {
+        logger.error("InvalidCredentialException 발생", e)
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
             ErrorResponseDto(
                 e.message,
@@ -41,6 +49,7 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponseDto> {
+        logger.error("MethodArgumentNotValidException 발생", e)
         val errors = e.bindingResult.fieldErrors.joinToString(separator = ", ") { "${it.field}: ${it.defaultMessage}" }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDto(errors)) // (e.message)보다 이렇게 하는게 더 스웨거에서 깔끔하더라
     }
