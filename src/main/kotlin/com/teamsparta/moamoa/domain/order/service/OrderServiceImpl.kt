89dee8293058eb97b412fdd7a583dfd0d957d67d
Karox1234 <zzzcomet@gmail.com>
2024-03-31 23:28:52 +0900

    수정 : 재고 복구 로직 수정

--- src/main/kotlin/com/teamsparta/moamoa/domain/order/service/OrderServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/order/service/OrderServiceImpl.kt
@@ -445,11 +445,11 @@ class OrderServiceImpl(
     override fun trollOrderDelete(orderUId: String) {
         val order = orderRepository.findByOrderUidAndDeletedAtIsNull(orderUId) ?: throw Exception()
         val payment = paymentRepository.findByIdOrNull(order.payment.id)
-        val stock = productStockRepository.findById(order.product.id!!) .orElseThrow()
+        val stock = productStockRepository.findByIdOrNull(order.product.id!!)
 
         order.deletedAt = LocalDateTime.now()
         payment!!.deletedAt = LocalDateTime.now()
-        stock.stock += order.quantity
+        stock!!.stock += order.quantity
     }
 
     @Transactional
