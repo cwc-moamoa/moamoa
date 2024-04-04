5db9cc4d020228477a26ec26d971e7c4f49effce
Karox1234 <zzzcomet@gmail.com>
2024-04-04 17:38:11 +0900

    수정 : 트랜잭션 위치 변경 , 결제가 완료되야 stock이 차감되게 변경, 결제 완료시 deletedAt이 null로 변경

--- src/main/kotlin/com/teamsparta/moamoa/domain/payment/service/PaymentServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/moamoa/domain/payment/service/PaymentServiceImpl.kt
@@ -6,6 +6,7 @@ import com.siot.IamportRestClient.request.CancelData
 import com.siot.IamportRestClient.response.IamportResponse
 import com.siot.IamportRestClient.response.Payment
 import com.teamsparta.moamoa.domain.order.repository.OrderRepository
+import com.teamsparta.moamoa.domain.order.service.OrderService
 import com.teamsparta.moamoa.domain.payment.dto.PaymentCallbackRequest
 import com.teamsparta.moamoa.domain.payment.dto.RequestPayDto
 import com.teamsparta.moamoa.domain.payment.model.PaymentStatus
@@ -21,6 +22,7 @@ import java.math.BigDecimal
 import java.time.LocalDateTime
 
 @Service
+@Transactional
 class PaymentServiceImpl(
     private val orderRepository: OrderRepository,
     private val paymentRepository: PaymentRepository,
@@ -29,11 +31,10 @@ class PaymentServiceImpl(
     private val applicationEventPublisher: ApplicationEventPublisher,
     private val redisService: RedisService,
 ) : PaymentService {
-    @Transactional
+
     override fun findRequestDto(orderUid: String): RequestPayDto {
-        val order =
-            orderRepository.findOrderAndPaymentAndSocialUser(orderUid)
-                .orElseThrow { IllegalArgumentException("주문이 없습니다.") }
+        val order = orderRepository.findOrderAndPaymentAndSocialUser(orderUid)
+            .orElseThrow { IllegalArgumentException("주문이 없습니다.") }
 
         return RequestPayDto(
             buyerName = order.socialUser.nickname,
@@ -50,23 +51,21 @@ class PaymentServiceImpl(
             // 결제 단건 조회(포트원) -> 포트원과 통신해서 고유UID 두개를 받아옴
             val iamportResponse = iamportClient.paymentByImpUid(request.paymentUid)
 
-            val order =
-                orderRepository.findOrderAndPayment(request.orderUid)
-                    .orElseThrow { IllegalArgumentException("주문 내역이 없습니다.") }
+            val order = orderRepository.findOrderAndPayment(request.orderUid)
+                .orElseThrow { IllegalArgumentException("주문 내역이 없습니다.") }
 
-            val stockFindAndPlus =
-                productStockRepository.findById(order.product.id!!)
-                    .orElseThrow { IllegalArgumentException("재고를 찾을 수 없습니다.") }
+            val stockFind = productStockRepository.findById(order.product.id!!)
+                .orElseThrow { IllegalArgumentException("재고를 찾을 수 없습니다.") }
 
             if (iamportResponse.response.status != "paid") {
-                order.deletedAt = LocalDateTime.now()
-                orderRepository.save(order)
-
-                order.payment.deletedAt = LocalDateTime.now()
-                paymentRepository.save(order.payment)
-
-                stockFindAndPlus.stock += order.quantity
-                productStockRepository.save(stockFindAndPlus)
+//                order.deletedAt = LocalDateTime.now()
+//                orderRepository.save(order)
+//
+//                order.payment.deletedAt = LocalDateTime.now()
+//                paymentRepository.save(order.payment)
+//
+//                stockFind.stock += order.quantity
+//                productStockRepository.save(stockFindAndPlus)
 
                 throw RuntimeException("결제 미완료")
             }
@@ -76,14 +75,14 @@ class PaymentServiceImpl(
             val iamportPrice = iamportResponse.response.amount.toDouble()
 
             if (iamportPrice != price) {
-                order.deletedAt = LocalDateTime.now()
-                orderRepository.save(order)
-
-                order.payment.deletedAt = LocalDateTime.now()
-                paymentRepository.save(order.payment)
-
-                stockFindAndPlus.stock += order.quantity
-                productStockRepository.save(stockFindAndPlus)
+//                order.deletedAt = LocalDateTime.now()
+//                orderRepository.save(order)
+//
+//                order.payment.deletedAt = LocalDateTime.now()
+//                paymentRepository.save(order.payment)
+//
+//                stockFind.stock += order.quantity
+//                productStockRepository.save(stockFindAndPlus)
 
                 // 결제금액 위변조로 의심되는 결제금액을 취소(포트원)
                 iamportClient.cancelPaymentByImpUid(
@@ -97,8 +96,12 @@ class PaymentServiceImpl(
                 throw RuntimeException("결제금액 위변조 의심")
             }
 
-            val paymentChange = order.payment.changePaymentBySuccess(PaymentStatus.OK, iamportResponse.response.impUid)
-            paymentRepository.save(paymentChange)
+            order.payment.changePaymentBySuccess(PaymentStatus.OK, iamportResponse.response.impUid)
+            order.deletedAt = null
+            order.payment.deletedAt = null
+            stockFind!!.stock -= order.quantity
+//            orderService.ifPaymentSuccessOrderChangeAndDeletedAtChange(order.orderUid!!)
+
 
             if (order.discount > 0.0) {
                 redisService.saveToRedis(
